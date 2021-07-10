4d1da172523236bfc50af45c1a78a2ab4ef96da1
siddhesh <iamsiddhesh22@gmail.com>
2021-07-10 20:09:06 +0530

    database connection

---
+++ main/src/DB.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DB {
+    Connection con=null;
+    java.sql.PreparedStatement pst;
+
+    public static Connection dbconnect(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/car_rental_management","root","");
+            return conn;
+        }catch(Exception e1){
+            System.out.println(e1);
+            return null;
+        }
+    }
+}
